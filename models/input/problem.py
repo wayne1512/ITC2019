5cb06133dbbda93a590b8b383853763c94369381
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-10-17 10:44:24 +0200

    made problem attributes final

--- models/input/problem.py
+++ models/input/problem.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 from models.input.course import Course
 from models.input.distribution import Distribution
 from models.input.optimization import Optimization
@@ -6,13 +8,14 @@ from models.input.student import Student
 
 
 class Problem:
-    def __init__(self, name, nrDays, slotsPerDay, nrWeeks, optimization:Optimization, rooms:list[Room], courses:list[Course], distributions:list[Distribution], students:list[Student]):
-        self.name = name
-        self.nrDays = nrDays
-        self.slotsPerDay = slotsPerDay
-        self.nrWeeks = nrWeeks
-        self.optimization = optimization
-        self.rooms = rooms
-        self.courses = courses
-        self.distributions = distributions
-        self.students = students
+    def __init__(self, name, nr_days, slots_per_day, nr_weeks, optimization: Optimization, rooms: list[Room],
+                 courses: list[Course], distributions: list[Distribution], students: list[Student]):
+        self.name: Final = name
+        self.nrDays: Final = nr_days
+        self.slotsPerDay: Final = slots_per_day
+        self.nrWeeks: Final = nr_weeks
+        self.optimization: Final = optimization
+        self.rooms: Final = rooms
+        self.courses: Final = courses
+        self.distributions: Final = distributions
+        self.students: Final = students
