9ac946c50240dc0e816457983baf893cbd254133
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-10-31 23:39:10 +0100

    same and diff rooms distributions

--- costCalcuation/distributions/create_distribtion_helper.py
+++ costCalcuation/distributions/create_distribtion_helper.py
@@ -1,10 +1,12 @@
 from costCalcuation.distributions.different_days_distribution_helper import DifferentDaysDistributionHelper
+from costCalcuation.distributions.different_room_distribution_helper import DifferentRoomDistributionHelper
 from costCalcuation.distributions.different_time_distribution_helper import DifferentTimeDistributionHelper
 from costCalcuation.distributions.different_weeks_distribution_helper import DifferentWeeksDistributionHelper
 from costCalcuation.distributions.not_implemented_distribution_helper import NotImplementedDistributionHelper
 from costCalcuation.distributions.not_overlap_distribution_helper import NotOverlapDistributionHelper
 from costCalcuation.distributions.overlap_distribution_helper import OverlapDistributionHelper
 from costCalcuation.distributions.same_days_distribution_helper import SameDaysDistributionHelper
+from costCalcuation.distributions.same_room_distribution_helper import SameRoomDistributionHelper
 from costCalcuation.distributions.same_start_distribution_helper import SameStartDistributionHelper
 from costCalcuation.distributions.same_time_distribution_helper import SameTimeDistributionHelper
 from costCalcuation.distributions.same_weeks_distribution_helper import SameWeeksDistributionHelper
@@ -40,5 +42,11 @@ def create_helper_for_distribution(problem: Problem, distribution: Distribution)
 
     if dist_type == "NotOverlap":
         return NotOverlapDistributionHelper(problem, distribution)
+
+    if dist_type == "SameRoom":
+        return SameRoomDistributionHelper(problem, distribution)
+
+    if dist_type == "DifferentRoom":
+        return DifferentRoomDistributionHelper(problem, distribution)
 
     return NotImplementedDistributionHelper(problem, distribution)
