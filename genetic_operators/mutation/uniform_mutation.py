9c7f4b01b267e2feffcfea34801a3cc90d1950d4
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-12-20 13:09:47 +0100

    remove closed_room_time_combinations constraints

--- genetic_operators/mutation/uniform_mutation.py
+++ genetic_operators/mutation/uniform_mutation.py
@@ -8,6 +8,6 @@ class UniformMutation:
     def __init__(self, chance=0.001):
         self.chance = chance
 
-    def mutate(self, gene, max_gene, problem):
+    def mutate(self, gene, max_gene):
         choice = np.random.rand(*gene.shape) < self.chance
-        return np.where(choice, random_gene(max_gene, problem), gene)
+        return np.where(choice, random_gene(max_gene), gene)
